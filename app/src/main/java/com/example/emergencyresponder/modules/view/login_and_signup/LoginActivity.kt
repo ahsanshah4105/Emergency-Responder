da521bd9f912e4bbdde4242ca5773cc233b0687f
Ahsan Ali Shah <ahsanalishah4105@gmail.com>
2025-11-02 21:09:46 +0500

    Login, Sign Up and Forgot Password design is created

---
+++ app/src/main/java/com/example/emergencyresponder/modules/view/login_and_signup/LoginActivity.kt
@@ -0,0 +1,58 @@
+package com.example.emergencyresponder.modules.view.login_and_signup
+
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.emergencyresponder.R
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var checkBox: CheckBox
+    private lateinit var forgotPassword: TextView
+    private lateinit var signUp: LinearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        initializeViews()
+        setupCheckbox()
+        handleIntent()
+    }
+
+    private fun initializeViews() {
+        checkBox = findViewById(R.id.checkbox)
+        forgotPassword = findViewById(R.id.forgot_password)
+        signUp = findViewById(R.id.signUp_layout)
+    }
+
+    private fun setupCheckbox() {
+        val uncheckedColor = ContextCompat.getColor(this, R.color.hint_color)
+        val checkedColor = ContextCompat.getColor(this, R.color.alert)
+
+        checkBox.buttonTintList = ColorStateList.valueOf(uncheckedColor)
+
+        checkBox.setOnCheckedChangeListener { _, isChecked ->
+            val color = if (isChecked) checkedColor else uncheckedColor
+            checkBox.buttonTintList = ColorStateList.valueOf(color)
+        }
+    }
+
+    private fun handleIntent() {
+        forgotPassword.setOnClickListener {
+            val intent = Intent(this, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+        signUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/emergencyresponder/modules/view/login_and_signup/SignUpActivity.kt
@@ -0,0 +1,33 @@
+package com.example.emergencyresponder.modules.view.login_and_signup
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.emergencyresponder.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var loginButton: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up)
+        initializeViews()
+        handleIntent()
+    }
+
+    private fun initializeViews() {
+        loginButton = findViewById(R.id.login_button)
+
+    }
+
+    private fun handleIntent() {
+        loginButton.setOnClickListener {
+            finish()
+        }
+    }
+}
